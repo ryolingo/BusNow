f4a9f5101089a9b50491576beae1e6d966142c1a
ryolingo <ryomatsuya04@gmail.com>
2025-02-20 15:33:00 +0900

    Initial Commit

---
+++ BusNow/BusNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusNowApp.swift
+//  BusNow
+//
+//  Created by matsumotoryota on 2025/02/20.
+//
+
+import SwiftUI
+
+@main
+struct BusNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
