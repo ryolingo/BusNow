ad2f39600844786ed7dfe4c6414a54337f8ca848
ryolingo <ryomatsuya04@gmail.com>
2025-03-09 00:15:41 +0900

    アナログ時計の追加

---
+++ BusNow/Views/ClockFaceView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+//アナログ時計のUIの部分
+struct ClockFaceView: View {
+    @ObservedObject var viewModel: ClockViewModel
+    
+    var body: some View {
+        ZStack {
+            // 時計の文字盤
+            Circle()
+                .stroke(Color.gray.opacity(0.5), lineWidth: 2)
+                .frame(width: 200, height: 200)
+            
+            // 時間のメモリ
+            ForEach(0..<60) { minute in
+                let style = viewModel.getMarkerStyle(for: minute)
+                Rectangle()
+                    .fill(style.color)
+                    .frame(width: 2, height: style.length)
+                    .offset(y: -90 + style.length / 2)
+                    .rotationEffect(.degrees(Double(minute) * 6))
+            }
+            
+            // 現在時刻の針
+            ClockHand(date: viewModel.currentTime, color: .blue, handType: .hour)
+            ClockHand(date: viewModel.currentTime, color: .blue, handType: .minute)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ClockFaceView(viewModel: ClockViewModel())
+}
